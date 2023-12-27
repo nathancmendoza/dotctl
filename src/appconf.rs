ce899bd0483f95b9a5025fc07da953224feb1a80
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-12-26 16:08:31 -0800

    Incorporate strategic resolution

--- src/appconf.rs
+++ src/appconf.rs
@@ -8,11 +8,18 @@ pub mod links {
 
     use dirs::home_dir;
 
+    const HOME_PREFIX: &str = "~/";
+
+    fn use_resolver<P: AsRef<Path>>(to_resolve: P, resolver: impl LinkResolver) -> Result<PathBuf, LinkResolutionError> {
+        resolver.resolve(to_resolve)
+    }
+
+
+
     trait LinkResolver {
 
         fn resolve<P: AsRef<Path>>(&self, to_resolve: P) -> Result<PathBuf, LinkResolutionError>;
 
-        fn can_resolve<P: AsRef<Path>>(&self, to_resolve: P) -> bool;
     }
 
     #[derive(Debug, PartialEq, Eq)]
@@ -31,7 +38,14 @@ pub mod links {
     #[derive(Debug, Clone)]
     pub enum LinkResolutionError {
         NoHomeDirectoryFound,
-        NoHomeDirectoryToResolve
+        NoHomeDirectoryToResolve,
+        ResolverNotApplicable
+    }
+
+    #[derive(Debug, Clone)]
+    enum LinkResolutionIssue {
+        HasTilde,
+        IsRelative,
     }
 
     struct ResolveWithHomeDirectory<'a> {
@@ -39,49 +53,47 @@ pub mod links {
         home_prefix: &'a str
     }
 
-    struct ResolveWithParentDirectory<D: AsRef<Path>> {
-        parent: D
+    struct ResolveWithParentDirectory<'a, D: AsRef<Path>> {
+        parent: &'a Option<D>
     }
 
-    struct ResolveWithRootDirectory{}
+//    struct ResolveWithRootDirectory{}
+//
+//    impl ResolveWithRootDirectory {
+//        pub fn new() -> Self {
+//            ResolveWithRootDirectory{}
+//        }
+//    }
+//
+//    impl LinkResolver for ResolveWithRootDirectory {
+//        fn resolve<P: AsRef<Path>>(&self, to_resolve: P) -> Result<PathBuf, LinkResolutionError> {
+//            Ok(to_resolve.as_ref().to_path_buf())
+//        }
+//
+//        fn can_resolve<P: AsRef<Path>>(&self, to_resolve: P) -> bool {
+//            to_resolve.as_ref().to_path_buf().is_absolute()
+//        }
+//
+//    }
 
-    impl ResolveWithRootDirectory {
-        pub fn new() -> Self {
-            ResolveWithRootDirectory{}
-        }
-    }
-
-    impl LinkResolver for ResolveWithRootDirectory {
-        fn resolve<P: AsRef<Path>>(&self, to_resolve: P) -> Result<PathBuf, LinkResolutionError> {
-            Ok(to_resolve.as_ref().to_path_buf())
-        }
-
-        fn can_resolve<P: AsRef<Path>>(&self, to_resolve: P) -> bool {
-            to_resolve.as_ref().to_path_buf().is_absolute()
-        }
-
-    }
-
-    impl<D: AsRef<Path>> ResolveWithParentDirectory<D> {
-        pub fn new(root: D) -> Self {
+    impl<'a, D: AsRef<Path>> ResolveWithParentDirectory<'a, D> {
+        pub fn new(root: &'a Option<D>) -> Self {
             ResolveWithParentDirectory { parent: root }
         }
     }
 
-    impl<D: AsRef<Path>> LinkResolver for ResolveWithParentDirectory<D> {
+    impl<'a, D: AsRef<Path>> LinkResolver for ResolveWithParentDirectory<'a, D> {
         fn resolve<P: AsRef<Path>>(&self, to_resolve: P) -> Result<PathBuf, LinkResolutionError> {
-            Ok(self.parent.as_ref().to_path_buf().join(to_resolve.as_ref().to_path_buf()))
-        }
-
-        fn can_resolve<P: AsRef<Path>>(&self, to_resolve: P) -> bool {
-            let suffix = to_resolve.as_ref().to_path_buf();
-            suffix.is_relative() && !suffix.starts_with(&self.parent)
+            match &self.parent {
+                Some(p) => Ok(p.as_ref().to_path_buf().join(to_resolve.as_ref().to_path_buf())),
+                None => Err(LinkResolutionError::ResolverNotApplicable)
+            }
         }
     }
 
     impl<'a> ResolveWithHomeDirectory<'a>{
         pub fn new() -> Self {
-            ResolveWithHomeDirectory {home_dir: home_dir(), home_prefix: "~/"}
+            ResolveWithHomeDirectory {home_dir: home_dir(), home_prefix: HOME_PREFIX}
         }
     }
 
@@ -92,17 +104,10 @@ pub mod links {
                 Some(h) => {
                     match to_resolve.as_ref().to_path_buf().strip_prefix(self.home_prefix) {
                         Err(_) => Err(LinkResolutionError::NoHomeDirectoryToResolve),
-                        Ok(suffix) => {
-                            Ok(h.join(suffix))
-                        }
+                        Ok(suffix) => Ok(h.join(suffix))
                     }
                 }
             }
-        }
-
-        fn can_resolve<P: AsRef<Path>>(&self, to_resolve: P) -> bool {
-            let suffix = to_resolve.as_ref().to_path_buf();
-            suffix.starts_with(self.home_prefix)
         }
     }
 
@@ -110,7 +115,8 @@ pub mod links {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             match self {
                 LinkResolutionError::NoHomeDirectoryFound => write!(f, "The `~` symbol could not be resolved to a path"),
-                LinkResolutionError::NoHomeDirectoryToResolve => write!(f, "Expected to resolve `~` prefix but was not found")
+                LinkResolutionError::NoHomeDirectoryToResolve => write!(f, "Expected to resolve `~` prefix but was not found"),
+                LinkResolutionError::ResolverNotApplicable => write!(f, "The chosen resolver cannot be applied")
             }
         }
     }
@@ -127,22 +133,58 @@ pub mod links {
             }
         }
         
-        pub fn get_canonical_source(&self, parent: Option<P>) -> Result<PathBuf, LinkResolutionError> {
-            unimplemented!() 
+        pub fn get_resolved_source(&self, parent: Option<P>) -> Result<PathBuf, LinkResolutionError> {
+            let mut true_source = self.source.as_ref().to_path_buf();
+            while let Some(issue) = self.find_source_resolutions(&true_source) {
+                let resolved_source = match issue {
+                    LinkResolutionIssue::HasTilde => use_resolver(true_source, ResolveWithHomeDirectory::new()),
+                    LinkResolutionIssue::IsRelative => use_resolver(true_source, ResolveWithParentDirectory::new(&parent))
+                }?;
+                true_source = resolved_source;
+            }
+            Ok(true_source)
         }
 
-        pub fn get_canonical_target(&self) -> Result<PathBuf, LinkResolutionError> {
-            unimplemented!()
+        pub fn get_resolved_target(&self) -> Result<PathBuf, LinkResolutionError> {
+            let mut true_target = self.target.as_ref().to_path_buf();
+            while let Some(issue) = self.find_target_resolutions(&true_target) {
+                let resolved_target = match issue {
+                    LinkResolutionIssue::HasTilde => use_resolver(true_target, ResolveWithHomeDirectory::new()),
+                    LinkResolutionIssue::IsRelative => {
+                        let no_parent: Option<&str> = None;
+                        use_resolver(true_target, ResolveWithParentDirectory::new(&no_parent))
+                    }
+                }?;
+                true_target = resolved_target;
+            }
+            Ok(true_target)
         }
 
         pub fn get_link_strategy(&self) -> &LinkMode {
             &self.link_mode
         }
 
-        fn resolve_link_path(&self, to_resolve: P) -> Result<PathBuf, LinkResolutionError> {
+        fn find_source_resolutions(&self, to_resolve: &PathBuf) -> Option<LinkResolutionIssue> {
             
+            if to_resolve.starts_with(HOME_PREFIX) {
+                return Some(LinkResolutionIssue::HasTilde);
+            }
+
+            if to_resolve.is_relative() {
+                return Some(LinkResolutionIssue::IsRelative);
+            }
+
+            None
         }
 
+        fn find_target_resolutions(&self, to_resolve: &PathBuf) -> Option<LinkResolutionIssue> {
+
+            if to_resolve.starts_with(HOME_PREFIX) {
+                return Some(LinkResolutionIssue::HasTilde);
+            }
+
+            None
+        }
     }
 }
 
