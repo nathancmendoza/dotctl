6f7850eb1b803dd3f181304ef9cf3652f8601534
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-01-16 15:00:30 -0800

    Configuration processing implemented with error handling

--- src/config.rs
+++ src/config.rs
@@ -3,9 +3,16 @@ extern crate serde;
 extern crate serde_yaml;
 
 use serde::{Serialize, Deserialize};
+use serde_yaml::from_reader;
 
 use crate::links::LinkSpec;
 use crate::hooks::HookSpec;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::fmt;
+
+const CONFIG_FILE: &str = "test.yaml";
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct DotfileConfiguration {
@@ -38,4 +45,36 @@ enum SystemName {
 enum ConfigStatus {
     READY,
     UNUSED,
+}
+
+#[derive(Debug)]
+pub enum ConfigurationError {
+    ParsingError(serde_yaml::Error),
+    NoConfigurationFound,
+}
+
+impl fmt::Display for ConfigurationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            // ConfigurationError::ConfigurationModified => write!(f, "The configuration is dirty"),
+            ConfigurationError::NoConfigurationFound => write!(f,"Configuration file not found"),
+            ConfigurationError::ParsingError(e) => write!(f, "{}", format!("The configuration has errors: {}", e))
+        }
+    }
+}
+
+impl std::error::Error for ConfigurationError {}
+
+pub fn read_config() -> Result<DotfileConfiguration, ConfigurationError> {
+    let file = match File::open(CONFIG_FILE) {
+        Ok(the_file) => the_file,
+        Err(_) => return Err(ConfigurationError::NoConfigurationFound)
+    };
+
+    let reader = BufReader::new(file);
+
+    match from_reader(reader) {
+        Ok(conf) => Ok(conf),
+        Err(e) => Err(ConfigurationError::ParsingError(e))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,16 @@
 
-extern crate serde;
-extern crate serde_yaml;
-
-use serde_yaml::from_reader;
-
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-
 mod config;
 mod links;
 mod hooks;
 
-use crate::config::DotfileConfiguration;
-
-fn read_yaml_from_file<P: AsRef<Path>>(path: P) -> DotfileConfiguration {
-    let file = match File::open(path) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("File opening error: {:?}", e);
-            panic!("Failed because of previous error.");
-        }
-    };
-
-    let reader = BufReader::new(file);
-
-    match from_reader(reader) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("Deserialization error: {:?}", e);
-            panic!("Failed because of previous error");
-        }
-    }
-}
+use crate::config::read_config;
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let value = read_yaml_from_file("test.yaml".to_owned());
-    println!("{:?}", value);
+    let _ = match read_config() {
+        Ok(conf) => println!("{:?}", conf),
+        Err(e) => println!("{}", e)
+    };
+
     Ok(())
 }
