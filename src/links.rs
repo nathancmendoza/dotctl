c8ca372d5e9f1ed8949966c3b31c54f983a48d26
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-01-20 12:54:16 -0800

    Implement link creation for appconfs

--- src/links.rs
+++ src/links.rs
@@ -10,6 +10,7 @@ use resolve_path::PathResolveExt;
 use std::path::Path;
 use std::io;
 use std::str::FromStr;
+use std::fs;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct LinkSpec {
@@ -41,16 +42,65 @@ impl FromStr for LinkMode {
     }
 }
 
-pub fn setup_link(link_info: &LinkSpec) {
+pub fn setup_link(link_info: &LinkSpec, link_repo: &str) -> Result<(), io::Error> {
+    let true_source = link_info.source.try_resolve_in(link_repo)?;
+    let true_target = link_info.target.try_resolve()?;
+
+    println!("Linking: {:?} -> {:?}", true_source, true_target);
+
+    if !true_source.exists() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, format!("No such source path: {:?}", true_source)));
+    }
+
+    match link_info.mode {
+        LinkMode::Soft => make_soft_link(&true_source, &true_target),
+        LinkMode::Hard => fs::hard_link(&true_source, &true_target),
+        LinkMode::Copy => make_copy(&true_source, &true_target)
+    }
     // 1. fully resolve target and source paths and verify source exists
     // 2. match against link mode
     //      softlink => create the softlink from target -> source
     //      hardlink => create the hardlink from target -> source
     //      copy => copy contents of target into source
-    unimplemented!()
 }
 
 pub fn teardown_link(link_info: &LinkSpec) {
     // 1. fully resolve target path and verify target exists
     // 2. remove the filesystem object at target
+}
+
+fn make_soft_link(source: &Path, target: &Path) -> Result<(), io::Error> {
+    if source.is_file() {
+        symlink_dir(source, target)
+    }
+    else if source.is_dir() {
+        symlink_file(source, target)
+    }
+    else {
+        Err(io::Error::new(io::ErrorKind::InvalidData, format!("Source must be a regular file or directory.")))
+    }
+}
+
+fn make_copy(source: &Path, target: &Path) -> Result<(), io::Error> {
+    if source.is_file() {
+        match fs::copy(source, target) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Interrupted, format!("Did not copy {:?} -> {:?} completely", source, target)))
+        }
+    }
+    else {
+        if !target.exists() {
+            fs::create_dir(target)?;
+        }
+
+        for entry in fs::read_dir(source)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+            let dest_path = target.join(entry.file_name());
+
+            make_copy(&entry_path, &dest_path)?;
+        }
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,20 @@ use clap::Parser;
 
 use crate::cli::DotterInvocation;
 use crate::config::read_config;
+use crate::links::setup_link;
 
 fn use_config() {
 
 }
 
-fn setup_config() {
-
+fn setup_config(app_conf: &str) -> Result<(), std::io::Error> {
+    let configs = read_config()?;
+    for item in configs.all_configs().filter(| config | *config.assigned_name() == app_conf).filter(| config | config.config_os() == std::env::consts::OS) {
+        for link in item.links() {
+            setup_link(link, configs.config_repo())?;
+        }
+    }
+    Ok(())
 }
 
 fn teardwon_config() {
@@ -40,10 +47,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         cli::DotterActionWord::Setup { app_conf, dry_run, link_mode } => {
             println!("Setup {} configuration on {}", app_conf, std::env::consts::OS);
             println!("Work will actually occur: {}", !dry_run);
-            let mut configs = read_config().unwrap();
-            for item in configs.all_configs().filter(| config | *config.assigned_name() == app_conf).filter(| config | config.config_os() == std::env::consts::OS) {
-                println!("{:?}", item);
-            }
+            setup_config(&app_conf)?;
         },
         cli::DotterActionWord::Teardown { app_conf, dry_run } => {
             println!("Teardown {} configuration on{}", app_conf, std::env::consts::OS);
