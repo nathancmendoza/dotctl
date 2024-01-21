caf72ea3debca936072a21c4dc57a8cdfb8f22e0
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-01-21 13:01:19 -0800

    Change symlink to automatic

--- src/links.rs
+++ src/links.rs
@@ -5,7 +5,7 @@ extern crate symlink;
 extern crate resolve_path;
 
 use serde::{Serialize, Deserialize};
-use symlink::{symlink_dir, symlink_file, remove_symlink_file, remove_symlink_dir};
+use symlink::{symlink_auto, remove_symlink_auto};
 use resolve_path::PathResolveExt;
 use std::path::Path;
 use std::io;
@@ -53,6 +53,12 @@ impl FromStr for LinkMode {
 }
 
 pub fn setup_link<P: AsRef<Path>>(link_info: &LinkSpec, link_repo: P) -> Result<(), io::Error> {
+    // 1. fully resolve target and source paths and verify source exists
+    // 2. match against link mode
+    //      softlink => create the softlink from target -> source
+    //      hardlink => create the hardlink from target -> source
+    //      copy => copy contents of target into source
+
     let true_source = link_info.source.try_resolve_in(link_repo.as_ref())?;
     let true_target = link_info.target.try_resolve()?;
 
@@ -63,32 +69,15 @@ pub fn setup_link<P: AsRef<Path>>(link_info: &LinkSpec, link_repo: P) -> Result<
     }
 
     match link_info.mode {
-        LinkMode::Soft => make_soft_link(&true_source, &true_target),
+        LinkMode::Soft => symlink_auto(&true_source, &true_target),
         LinkMode::Hard => fs::hard_link(&true_source, &true_target),
         LinkMode::Copy => make_copy(&true_source, &true_target)
     }
-    // 1. fully resolve target and source paths and verify source exists
-    // 2. match against link mode
-    //      softlink => create the softlink from target -> source
-    //      hardlink => create the hardlink from target -> source
-    //      copy => copy contents of target into source
 }
 
 pub fn teardown_link(link_info: &LinkSpec) {
     // 1. fully resolve target path and verify target exists
     // 2. remove the filesystem object at target
-}
-
-fn make_soft_link(source: &Path, target: &Path) -> Result<(), io::Error> {
-    if source.is_file() {
-        symlink_dir(source, target)
-    }
-    else if source.is_dir() {
-        symlink_file(source, target)
-    }
-    else {
-        Err(io::Error::new(io::ErrorKind::InvalidData, format!("Source must be a regular file or directory.")))
-    }
 }
 
 fn make_copy(source: &Path, target: &Path) -> Result<(), io::Error> {
